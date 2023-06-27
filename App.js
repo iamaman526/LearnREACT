97b1e7a8b9e138af765fa293358b142c85508741
iamaman526 <iamaman526@gmail.com>
2023-06-27 18:38:15 +0530

    jsx update

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// --------------------------- day 1 and 2 ---------------------------------//
+
 // creating one element through reactdom
 
 // const heading = React.createElement(
@@ -14,18 +16,36 @@ import ReactDOM from "react-dom/client";
 
 // create nested element using react dom.
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Namaste react"),
-    React.createElement("h2", {}, "I am H2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am H1 tag"),
-    React.createElement("h2", {}, "I am H2 tag"),
-  ]),
-]);
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "Namaste react"),
+//     React.createElement("h2", {}, "I am H2 tag"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "I am H1 tag"),
+//     React.createElement("h2", {}, "I am H2 tag"),
+//   ]),
+// ]);
 
-console.log(parent);
+// console.log(parent);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(parent);
+
+// ----------------------day 1 and 2 ends here---------------------------------//
+
+// REACT STARTS HERE ------------------------DAY 3 ----------------------------//
+
+// React.creatElement => Reactelement(object) => HTMLElement(render)
+
+const heading = (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX
+  </h1>
+);
+//JSX - HTML -like  or XML-like syntax
+//JSX =>   React.createelment => Reactelement(object) => HTMLElement(render)
+// it is a reactElement.
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(heading);
