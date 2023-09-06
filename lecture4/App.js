4608954c0dbe1ccc247a110ce7fe737e3f8a6c46
iamaman526 <iamaman526@gmail.com>
2023-09-06 20:53:53 +0530

    updated

--- lecture4/App.js
+++ lecture4/App.js
@@ -43,7 +43,10 @@ const Header = () => {
 
 const RestaurentCard = (props) => {
   const { resData } = props;
-  console.log(resData);
+  // console.log(resData);
+
+   const { cloudinaryImageId, name,area,costForTwo,cuisines}  = resData?.data?.data;
+
   return (
     <div
       className="res-card"
@@ -54,135 +57,364 @@ const RestaurentCard = (props) => {
       <img
         className="res-logo"
         alt="res-logo"
-        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/56c9ab92bd79745fd152a30fa2525426"
+        src= {"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"  +  cloudinaryImageId}
       />
-      <h3>{resData?.data?.data?.name}</h3>
+      <h3>{name}</h3>
      
-      <h4>{resData.data.data.area}</h4>
-      <h4>{resData?.data?.avgRating}</h4>
-      <h4>{resData?.data?.data.cuisines}</h4>
+      <h4>{area}</h4>
+      <h4>{costForTwo/100}</h4>
+      <h4>{cuisines}</h4>
       <h4>38 minutes</h4>
     </div>
   ); 
 };
 
 
-const resObj = 
- 
-  {
-    "cardType": "restaurant",
-    "layoutAlignmentType": "VERTICAL",
+const resList = [ 
+
+{
+  "cardType": "restaurant",
+  "layoutAlignmentType": "VERTICAL",
+  "data": {
+    "type": "restaurant",
     "data": {
-      "type": "restaurant",
-      "data": {
-        "type": "F",
-        "id": "731706",
-        "name": "Creams and berrys",
-        "uuid": "1deaba58-3e40-4b79-ac25-2f03bb96f756",
-        "city": "1",
-        "area": "Ramamurthy nagar",
-        "totalRatingsString": "",
-        "cloudinaryImageId": "a30ed459ccdc89cf1f3ab91def783582",
-        "cuisines": [
-          "Bakery"
+      "type": "F",
+      "id": "731707",
+      "name": "Creams and berrys",
+      "uuid": "1deaba58-3e40-4b79-ac25-2f03bb96f756",
+      "city": "1",
+      "area": "Ramamurthy nagar",
+      "totalRatingsString": "",
+      "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+      "cuisines": [
+        "Bakery"
+      ],
+      "tags": [
+        
+      ],
+      "costForTwo": 30000,
+      "costForTwoString": "₹300 FOR TWO",
+      "deliveryTime": 37,
+      "minDeliveryTime": 37,
+      "maxDeliveryTime": 37,
+      "slaString": "37 MINS",
+      "lastMileTravel": 0,
+      "slugs": {
+        "restaurant": "creams-and-berrys-ramamurthy-nagar-ramamurthy-nagar",
+        "city": "Bangalore"
+      },
+      "cityState": "1",
+      "address": "",
+      "locality": "Sbi Bank Colony",
+      "parentId": 434554,
+      "unserviceable": false,
+      "veg": false,
+      "select": false,
+      "favorite": false,
+      "tradeCampaignHeaders": [
+        
+      ],
+      "chain": [
+        
+      ],
+      "feeDetails": {
+        "fees": [
+          {
+            "name": "BASE_DISTANCE",
+            "fee": 9800,
+            "message": ""
+          },
+          {
+            "name": "BASE_TIME",
+            "fee": 0,
+            "message": ""
+          },
+          {
+            "name": "ANCILLARY_SURGE_FEE",
+            "fee": 0,
+            "message": ""
+          }
         ],
-        "tags": [
+        "totalFees": 9800,
+        "message": "",
+        "title": "Delivery Charge",
+        "amount": "9800",
+        "icon": ""
+      },
+      "availability": {
+        "opened": true,
+        "nextOpenMessage": "",
+        "nextCloseMessage": ""
+      },
+      "longDistanceEnabled": 0,
+      "rainMode": "NONE",
+      "thirdPartyAddress": false,
+      "thirdPartyVendor": "",
+      "adTrackingID": "cid=7387083~p=1~eid=00000189-7432-0d7e-22dc-875200d9014f~srvts=1689871584638~89290",
+      "badges": {
+        "imageBased": [
           
         ],
-        "costForTwo": 30000,
-        "costForTwoString": "₹300 FOR TWO",
+        "textBased": [
+          
+        ],
+        "textExtendedBadges": [
+          
+        ]
+      },
+      "lastMileTravelString": "9.5 kms",
+      "hasSurge": false,
+      "sla": {
+        "restaurantId": "",
         "deliveryTime": 37,
         "minDeliveryTime": 37,
         "maxDeliveryTime": 37,
-        "slaString": "37 MINS",
         "lastMileTravel": 0,
-        "slugs": {
-          "restaurant": "creams-and-berrys-ramamurthy-nagar-ramamurthy-nagar",
-          "city": "Bangalore"
-        },
-        "cityState": "1",
-        "address": "",
-        "locality": "Sbi Bank Colony",
-        "parentId": 434554,
-        "unserviceable": false,
-        "veg": false,
-        "select": false,
-        "favorite": false,
-        "tradeCampaignHeaders": [
-          
-        ],
-        "chain": [
-          
-        ],
-        "feeDetails": {
-          "fees": [
-            {
-              "name": "BASE_DISTANCE",
-              "fee": 9800,
-              "message": ""
-            },
-            {
-              "name": "BASE_TIME",
-              "fee": 0,
-              "message": ""
-            },
-            {
-              "name": "ANCILLARY_SURGE_FEE",
-              "fee": 0,
-              "message": ""
-            }
-          ],
-          "totalFees": 9800,
-          "message": "",
-          "title": "Delivery Charge",
-          "amount": "9800",
-          "icon": ""
-        },
-        "availability": {
-          "opened": true,
-          "nextOpenMessage": "",
-          "nextCloseMessage": ""
-        },
-        "longDistanceEnabled": 0,
+        "lastMileDistance": 9.5,
+        "serviceability": "SERVICEABLE",
         "rainMode": "NONE",
-        "thirdPartyAddress": false,
-        "thirdPartyVendor": "",
-        "adTrackingID": "cid=7387083~p=1~eid=00000189-7432-0d7e-22dc-875200d9014f~srvts=1689871584638~89290",
-        "badges": {
-          "imageBased": [
-            
-          ],
-          "textBased": [
-            
-          ],
-          "textExtendedBadges": [
-            
-          ]
-        },
-        "lastMileTravelString": "9.5 kms",
-        "hasSurge": false,
-        "sla": {
-          "restaurantId": "",
-          "deliveryTime": 37,
-          "minDeliveryTime": 37,
-          "maxDeliveryTime": 37,
-          "lastMileTravel": 0,
-          "lastMileDistance": 9.5,
-          "serviceability": "SERVICEABLE",
-          "rainMode": "NONE",
-          "preferentialService": false,
-          "iconType": "EMPTY"
-        },
-        "promoted": true,
-        "avgRating": "--",
-        "totalRatings": 0,
-        "new": false
+        "preferentialService": false,
+        "iconType": "EMPTY"
       },
-      "subtype": "basic"
+      "promoted": true,
+      "avgRating": "--",
+      "totalRatings": 0,
+      "new": false
     },
-    "parentWidget": false
-  }
+    "subtype": "basic"
+  },
+  "parentWidget": false
+},
+
+{
+  "cardType": "restaurant",
+  "layoutAlignmentType": "VERTICAL",
+  "data": {
+    "type": "restaurant",
+    "data": {
+      "type": "F",
+      "id": "731706",
+      "name": "prince da dhaba",
+      "uuid": "1deaba58-3e40-4b79-ac25-2f03bb96f756",
+      "city": "1",
+      "area": "Ramamurthy nagar",
+      "totalRatingsString": "",
+      "cloudinaryImageId": "a30ed459ccdc89cf1f3ab91def783582",
+      "cuisines": [
+        "Bakery"
+      ],
+      "tags": [
+        
+      ],
+      "costForTwo": 30000,
+      "costForTwoString": "₹300 FOR TWO",
+      "deliveryTime": 37,
+      "minDeliveryTime": 37,
+      "maxDeliveryTime": 37,
+      "slaString": "37 MINS",
+      "lastMileTravel": 0,
+      "slugs": {
+        "restaurant": "creams-and-berrys-ramamurthy-nagar-ramamurthy-nagar",
+        "city": "Bangalore"
+      },
+      "cityState": "1",
+      "address": "",
+      "locality": "Sbi Bank Colony",
+      "parentId": 434554,
+      "unserviceable": false,
+      "veg": false,
+      "select": false,
+      "favorite": false,
+      "tradeCampaignHeaders": [
+        
+      ],
+      "chain": [
+        
+      ],
+      "feeDetails": {
+        "fees": [
+          {
+            "name": "BASE_DISTANCE",
+            "fee": 9800,
+            "message": ""
+          },
+          {
+            "name": "BASE_TIME",
+            "fee": 0,
+            "message": ""
+          },
+          {
+            "name": "ANCILLARY_SURGE_FEE",
+            "fee": 0,
+            "message": ""
+          }
+        ],
+        "totalFees": 9800,
+        "message": "",
+        "title": "Delivery Charge",
+        "amount": "9800",
+        "icon": ""
+      },
+      "availability": {
+        "opened": true,
+        "nextOpenMessage": "",
+        "nextCloseMessage": ""
+      },
+      "longDistanceEnabled": 0,
+      "rainMode": "NONE",
+      "thirdPartyAddress": false,
+      "thirdPartyVendor": "",
+      "adTrackingID": "cid=7387083~p=1~eid=00000189-7432-0d7e-22dc-875200d9014f~srvts=1689871584638~89290",
+      "badges": {
+        "imageBased": [
+          
+        ],
+        "textBased": [
+          
+        ],
+        "textExtendedBadges": [
+          
+        ]
+      },
+      "lastMileTravelString": "9.5 kms",
+      "hasSurge": false,
+      "sla": {
+        "restaurantId": "",
+        "deliveryTime": 37,
+        "minDeliveryTime": 37,
+        "maxDeliveryTime": 37,
+        "lastMileTravel": 0,
+        "lastMileDistance": 9.5,
+        "serviceability": "SERVICEABLE",
+        "rainMode": "NONE",
+        "preferentialService": false,
+        "iconType": "EMPTY"
+      },
+      "promoted": true,
+      "avgRating": "--",
+      "totalRatings": 0,
+      "new": false
+    },
+    "subtype": "basic"
+  },
+  "parentWidget": false
+},
+{
+  "cardType": "restaurant",
+  "layoutAlignmentType": "VERTICAL",
+  "data": {
+    "type": "restaurant",
+    "data": {
+      "type": "F",
+      "id": "731706",
+      "name": "prince da dhaba",
+      "uuid": "1deaba58-3e40-4b79-ac25-2f03bb96f756",
+      "city": "1",
+      "area": "Ramamurthy nagar",
+      "totalRatingsString": "",
+      "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      "cuisines": [
+        "Bakery"
+      ],
+      "tags": [
+        
+      ],
+      "costForTwo": 30000,
+      "costForTwoString": "₹300 FOR TWO",
+      "deliveryTime": 37,
+      "minDeliveryTime": 37,
+      "maxDeliveryTime": 37,
+      "slaString": "37 MINS",
+      "lastMileTravel": 0,
+      "slugs": {
+        "restaurant": "creams-and-berrys-ramamurthy-nagar-ramamurthy-nagar",
+        "city": "Bangalore"
+      },
+      "cityState": "1",
+      "address": "",
+      "locality": "Sbi Bank Colony",
+      "parentId": 434554,
+      "unserviceable": false,
+      "veg": false,
+      "select": false,
+      "favorite": false,
+      "tradeCampaignHeaders": [
+        
+      ],
+      "chain": [
+        
+      ],
+      "feeDetails": {
+        "fees": [
+          {
+            "name": "BASE_DISTANCE",
+            "fee": 9800,
+            "message": ""
+          },
+          {
+            "name": "BASE_TIME",
+            "fee": 0,
+            "message": ""
+          },
+          {
+            "name": "ANCILLARY_SURGE_FEE",
+            "fee": 0,
+            "message": ""
+          }
+        ],
+        "totalFees": 9800,
+        "message": "",
+        "title": "Delivery Charge",
+        "amount": "9800",
+        "icon": ""
+      },
+      "availability": {
+        "opened": true,
+        "nextOpenMessage": "",
+        "nextCloseMessage": ""
+      },
+      "longDistanceEnabled": 0,
+      "rainMode": "NONE",
+      "thirdPartyAddress": false,
+      "thirdPartyVendor": "",
+      "adTrackingID": "cid=7387083~p=1~eid=00000189-7432-0d7e-22dc-875200d9014f~srvts=1689871584638~89290",
+      "badges": {
+        "imageBased": [
+          
+        ],
+        "textBased": [
+          
+        ],
+        "textExtendedBadges": [
+          
+        ]
+      },
+      "lastMileTravelString": "9.5 kms",
+      "hasSurge": false,
+      "sla": {
+        "restaurantId": "",
+        "deliveryTime": 37,
+        "minDeliveryTime": 37,
+        "maxDeliveryTime": 37,
+        "lastMileTravel": 0,
+        "lastMileDistance": 9.5,
+        "serviceability": "SERVICEABLE",
+        "rainMode": "NONE",
+        "preferentialService": false,
+        "iconType": "EMPTY"
+      },
+      "promoted": true,
+      "avgRating": "--",
+      "totalRatings": 0,
+      "new": false
+    },
+    "subtype": "basic"
+  },
+  "parentWidget": false
+}
+]
  
+
 
 
 
@@ -191,9 +423,11 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurentCard resData = {resObj}/>
-        <RestaurentCard resData = {resObj}/>
-        <RestaurentCard resData = {resObj}/>
+        <RestaurentCard resData = {resList[0]}/>
+        <RestaurentCard resData = {resList[1]}/>
+        <RestaurentCard resData = {resList[2]}/>
+    
+      
       </div>
     </div>
   );
