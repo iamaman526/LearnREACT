27f82c508c81736255017b18e7764bf00e868294
iamaman526 <iamaman526@gmail.com>
2023-09-21 15:21:18 +0530

    react-fiber-done

--- lecture4/src/components/Body.js
+++ lecture4/src/components/Body.js
@@ -1,79 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import RestaurentCard from "./RestaurentCard";
-// import resList from "../utils/mockData";
+import resList from "../utils/mockData";
 
 const Body = () => {
-  // State Variable - Super powerful Variable
+  //Local State Variable - Super powerful Variable
+  const [RestaurentList, setRestaurentList] = useState(resList); //destructure
+
+  // const arr = useState(resList);
+  // const RestaurentList = arr[0];
+  // const setRestaurentList = arr[1];
 
   //Normal js variables
-  const RestaurentList = [
-    {
-      cardType: "restaurant",
-      layoutAlignmentType: "VERTICAL",
-      data: {
-        type: "restaurant",
-        data: {
-          type: "F",
-          id: "731703",
-          name: "prince da dhaba",
-          uuid: "1deaba58-3e40-4b79-ac25-2f03bb96f756",
-          city: "1",
-          ar: 4.0,
-          area: "Ramamurthy nagar",
-          totalRatingsString: "",
-          cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
-          cuisines: ["Bakery"],
-          tags: [],
-          costForTwo: 30000,
-          costForTwoString: "₹300 FOR TWO",
-        },
-      },
-    },
-    {
-      cardType: "restaurant",
-      layoutAlignmentType: "VERTICAL",
-      data: {
-        type: "restaurant",
-        data: {
-          type: "F",
-          id: "731709",
-          name: "jagdish ki dukan",
-          uuid: "1deaba58-3e40-4b79-ac25-2f03bb96f756",
-          city: "1",
-          ar: 3.8,
-          area: "Ramamurthy nagar",
-          totalRatingsString: "",
-          cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
-          cuisines: ["Bakery"],
-          tags: [],
-          costForTwo: 30000,
-          costForTwoString: "₹300 FOR TWO",
-        },
-      },
-    },
-    {
-      cardType: "restaurant",
-      layoutAlignmentType: "VERTICAL",
-      data: {
-        type: "restaurant",
-        data: {
-          type: "F",
-          id: "731409",
-          name: "manish ki shop",
-          uuid: "1deaba58-3e40-4b79-ac25-2f03bb96f756",
-          city: "1",
-          ar: 4.5,
-          area: "Ramamurthy nagar",
-          totalRatingsString: "",
-          cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
-          cuisines: ["Bakery"],
-          tags: [],
-          costForTwo: 30000,
-          costForTwoString: "₹300 FOR TWO",
-        },
-      },
-    },
-  ];
+  // const RestaurentListjs = [
+  //   {
+  //     cardType: "restaurant",
+  //     layoutAlignmentType: "VERTICAL",
+  //     data: {
+  //       type: "restaurant",
+  //       data: {
+  //         type: "F",
+  //         id: "731703",
+  //         name: "prince da dhaba",
+  //         uuid: "1deaba58-3e40-4b79-ac25-2f03bb96f756",
+  //         city: "1",
+  //         ar: 4.0,
+  //         area: "Ramamurthy nagar",
+  //         totalRatingsString: "",
+  //         cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+  //         cuisines: ["Bakery"],
+  //         tags: [],
+  //         costForTwo: 30000,
+  //         costForTwoString: "₹300 FOR TWO",
+  //       },
+  //     },
+  //   },
+  //   {.
+  //     cardType: "restaurant",
+  //     layoutAlignmentType: "VERTICAL",
+  //     data: {
+  //       type: "restaurant",
+  //       data: {
+  //         type: "F",
+  //         id: "731709",
+  //         name: "jagdish ki dukan",
+  //         uuid: "1deaba58-3e40-4b79-ac25-2f03bb96f756",
+  //         city: "1",
+  //         ar: 3.8,
+  //         area: "Ramamurthy nagar",
+  //         totalRatingsString: "",
+  //         cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+  //         cuisines: ["Bakery"],
+  //         tags: [],
+  //         costForTwo: 30000,
+  //         costForTwoString: "₹300 FOR TWO",
+  //       },
+  //     },
+  //   },
+  //   {
+  //     cardType: "restaurant",
+  //     layoutAlignmentType: "VERTICAL",
+  //     data: {
+  //       type: "restaurant",
+  //       data: {
+  //         type: "F",
+  //         id: "731409",
+  //         name: "manish ki shop",
+  //         uuid: "1deaba58-3e40-4b79-ac25-2f03bb96f756",
+  //         city: "1",
+  //         ar: 4.5,
+  //         area: "Ramamurthy nagar",
+  //         totalRatingsString: "",
+  //         cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+  //         cuisines: ["Bakery"],
+  //         tags: [],
+  //         costForTwo: 30000,
+  //         costForTwoString: "₹300 FOR TWO",
+  //       },
+  //     },
+  //   },
+  // ];
+
   return (
     <div className="body">
       <div className="filter">
@@ -83,6 +89,7 @@ const Body = () => {
             const filtered = RestaurentList.filter((x) => x.data.data.ar > 3.8);
 
             console.log(filtered);
+            setRestaurentList(filtered);
           }}
         >
           Top Rated Restaurent
