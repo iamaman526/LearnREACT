e9678fa93b901df90aeca935844e8fd62740b19e
iamaman526 <iamaman526@gmail.com>
2023-12-13 00:20:28 +0530

    shimmer-done

--- lecture4/src/components/Header.js
+++ lecture4/src/components/Header.js
@@ -1,26 +1,35 @@
 import React from "react";
 import { LOGO_URL } from "../utils/constants";
-
+import { useState } from "react";
 
 const Header = () => {
-    return (
-      <div className="header">
-        <div className="logo-container">
-          <img
-            className="logo"
-            src= {LOGO_URL}
-          />
-        </div>
-        <div className="nav-item">
-          <ul>
-            <li>Home</li>
-            <li>About us </li>
-            <li>Contact us</li>
-            <li>Cart</li>
-          </ul>
-        </div>
-      </div>
-    );
-  };
+  const [btnNameReact , setbtnNameReact] = useState(["Login"]);
 
-  export default Header;
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
+      </div>
+      <div className="nav-item">
+        <ul>
+          <li>Home</li>
+          <li>About us </li>
+          <li>Contact us</li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              setbtnNameReact("Logout");
+              
+              console.log(btnNameReact);
+            }}
+          >
+            {btnNameReact}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
